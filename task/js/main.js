df92470d8e91e9266e055bacaabb6107f437ed41
vcinonyx <vcinonyx@gmail.com>
2020-12-25 14:30:12 +0200

    extra spaces rm

--- task/js/main.js
+++ task/js/main.js
@@ -1,3 +1,9 @@
+const matchFrom = (str, reg, fromIndex)  => {
+    for (const match of str.matchAll(reg)) {
+      if (match.index >= fromIndex) return match.index;
+    } return -1;
+}
+
 document.getElementById('filter-button').addEventListener("click", () => {
     let arr = document.getElementById('text').innerHTML.split(' ');
     let substr = document.getElementById('user-substr').value;
@@ -11,19 +17,12 @@ document.getElementById('filter-button').addEventListener("click", () => {
                 indexOfMatch  = matchFrom(word, regexp, indexOfMatch + 1);
                 word = arr[index] = word.substring(0, indexOfMatch) + word.substring(indexOfMatch + substr.length, word.length);
                 }
-            }
-        }
-    );    
+    }});    
 
     let result = arr.join(' ');
     document.getElementById('text').innerHTML = result;     
 })
 
-function matchFrom(str, reg, fromIndex) {
-    for (const match of str.matchAll(reg)) {
-      if (match.index >= fromIndex) return match.index;
-    } return -1;
-  }
 
 document.getElementById('add-text-button').addEventListener("click", () => {
     let new_text = document.getElementById('user-text').value;
